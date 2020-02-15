035b16faaa9bcda7190d12c0940bdd09f9da3619
ElienVissers <ElienVissers@users.noreply.github.com>
2020-02-15 14:44:56 +0100

    setup content @ MainScreen

--- src/Battleship/view/mainscreen/MainScreenView.java
+++ src/Battleship/view/mainscreen/MainScreenView.java
@@ -1,6 +1,8 @@
 package Battleship.view.mainscreen;
 
 import Battleship.view.UISettings;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.layout.*;
 import javafx.scene.control.*;
 
@@ -17,6 +19,10 @@ public class MainScreenView extends BorderPane  {
     private MenuItem optionsMI;
     private MenuItem highscoresMI;
     private CheckMenuItem computerPlayerCheck;
+    private Label player1Label;
+    private Label player2Label;
+    private TextField player1Text;
+    private TextField player2Text;
     private UISettings uiSettings;
 
     public MainScreenView(UISettings uiSettings) {
@@ -26,19 +32,47 @@ public class MainScreenView extends BorderPane  {
     }
 
     private void initialiseNodes() {
+        //MENU
         this.aboutMI = new MenuItem("About");
         this.infoMI = new MenuItem("Info");
         this.exitMI = new MenuItem("Exit");
         this.computerPlayerCheck = new CheckMenuItem("Virtual opponent");
         this.optionsMI = new MenuItem("Options");
         this.highscoresMI = new MenuItem("Highscores");
+        //CONTENT
+        this.player1Label = new Label("Player 1");
+        this.player2Label = new Label("Player 2");
+        this.player1Text = new TextField();
+        this.player2Text = new TextField();
+
     }
 
     private void layoutNodes() {
+        //MENU
         Menu helpM = new Menu("Help",null, aboutMI, infoMI, new SeparatorMenuItem(), exitMI);
         Menu optionsM = new Menu("Options",null, computerPlayerCheck, optionsMI, new SeparatorMenuItem(), highscoresMI);
         MenuBar menuBar = new MenuBar(optionsM, helpM);
         setTop(menuBar);
+        //CONTENT
+        VBox player1 = new VBox();
+        VBox player2 = new VBox();
+        layoutVBox(player1, player1Label, player1Text);
+        layoutVBox(player2, player2Label, player2Text);
+        HBox playerBox = new HBox();
+        layoutHBox(playerBox, player1, player2);
+        this.setCenter(playerBox);
+    }
+
+    private void layoutVBox(VBox box, Label label, TextField text) {
+        box.setAlignment(Pos.CENTER);
+        box.setSpacing(20);
+        box.getChildren().addAll(label, text);
+    }
+
+    private void layoutHBox(HBox box, VBox childBox1, VBox childBox2) {
+        box.getChildren().addAll(childBox1, childBox2);
+        box.setAlignment(Pos.CENTER);
+        box.setSpacing(200);
     }
 
     MenuItem getExitItem() {return exitMI;}
