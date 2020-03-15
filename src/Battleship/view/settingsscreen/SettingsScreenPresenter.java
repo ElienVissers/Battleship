e38babc3c01c391bcb0ad8ab932439932ef9fffa
Ibrizic <jan-basket@hotmail.com>
2020-03-15 17:14:14 +0100

    Added save alert and full implementation

--- src/Battleship/view/settingsscreen/SettingsScreenPresenter.java
+++ src/Battleship/view/settingsscreen/SettingsScreenPresenter.java
@@ -4,7 +4,14 @@ import Battleship.model.BattleshipModel;
 import Battleship.view.UISettings;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.Alert;
+import javafx.scene.control.DialogPane;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
+
+import java.nio.file.Files;
 
 /**
  * @author Elien Vissers-Similon
@@ -32,11 +39,6 @@ public class SettingsScreenPresenter {
     }
 
     private void updateView() {
-        view.getGridSizeSlider().getValue();
-        view.getFleetSizeSlider().getValue();
-        view.getFleetSizeSlider();
-        view.getGridSizeSlider();
-
     }
 
     private void addEventHandlers() {
@@ -44,14 +46,36 @@ public class SettingsScreenPresenter {
         // aan de controls uit de view.
         // Event handlers: roepen methodes aan uit het
         // model en zorgen voor een update van de view.
-        //TODO add method that saves the current settings and shows a message about fleetsize can't be higher than grid size
+        //TODO vragen over alert 2de if methode
         view.getConfirmButton().setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                event.consume();
+                if(view.getFleetSizeSlider().getValue() >= view.getGridSizeSlider().getValue()) {
+                    final Alert stopWindow = new Alert(Alert.AlertType.ERROR);
+                    try {
+                        DialogPane dialogPane = stopWindow.getDialogPane();
+                        dialogPane.setGraphic(new Label());
+                        dialogPane.getStylesheets().add(uiSettings.getStyleSheetPath().toUri().toURL().toString());
+                        dialogPane.getStyleClass().add("alert-window");
+                    } catch (Exception ignored) {}
+                    if (Files.exists(uiSettings.getApplicationIconPath())) {
+                        try {
+                            Stage stage = (Stage) stopWindow.getDialogPane().getScene().getWindow();
+                            stage.getIcons().add(new Image(uiSettings.getApplicationIconPath().toUri().toURL().toString()));
+                        } catch (Exception ignored) {}
+                    }
+                    stopWindow.setTitle("Warning!");
+                    stopWindow.setHeaderText("The selected fleet size is bigger than your universe (Grid) supports");
+                    stopWindow.setContentText("The fleet size has been adjust to fit in your universe");
+                    stopWindow.showAndWait();
+                    event.consume();
+                    model.setNumberOfShips((int) view.getGridSizeSlider().getValue());
+                }
+                model.setGridSize((int) view.getGridSizeSlider().getValue());
+                model.setNumberOfShips((int) view.getFleetSizeSlider().getValue());
             }
         });
-        //TODO add error message same as on close pane (That shows that settings won't be changed)
+        //TODO add error message same as on close pane (That shows that settings won't be changed. Are u sure you want to leave? Yes? No?) close view on yes
         view.getCancelButton().setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
@@ -60,7 +84,7 @@ public class SettingsScreenPresenter {
             });
     }
 
-    //TODO Add warning on close
+    //TODO same as above in view.getCancelButton().setOnAction()
     public void windowsHandler() {
         view.getScene().getWindow().setOnCloseRequest(new EventHandler<WindowEvent>() {
             @Override
